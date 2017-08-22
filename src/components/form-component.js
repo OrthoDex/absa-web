ff7e3fc90c5a4b7c1d3cad2333826553808c9e11
Ishaan Malhi <ishaan.malhi@gmail.com>
2017-08-22 20:44:15 +0530

    chore: Added build output js to gitignore

--- src/components/form-component.js
+++ src/components/form-component.js
@@ -12,6 +12,7 @@ export default class FormComponent extends React.Component {
 
     this._handleSubmit = this._handleSubmit.bind(this);
     this._submitStatus = this._submitStatus.bind(this);
+    this._helpTextStatus = this._helpTextStatus.bind(this);
 
   }
 
@@ -21,12 +22,21 @@ export default class FormComponent extends React.Component {
         <div className={`form-group ${this._submitStatus()}`}>
           <input className="form-control" placeholder="Enter the valid public YouTube Video Id" type="text" ref={(input) => this._videoUrl = input} />
           <span id="helpBlock" className="help-block">
-            <p className="text-danger">{this.state.message}</p>
+            <p className={`text-${this._helpTextStatus}`}>{this.state.message}</p>
           </span>
           <input className="btn btn-primary" type="submit" value="Submit" />
         </div>
       </form>
     );
+  }
+
+  _helpTextStatus() {
+    if(this.state.validInput == 0)
+      return "";
+    else if(this.state.validInput == 1)
+      return "success";
+    else
+      return "danger";
   }
 
   _submitStatus() {
@@ -50,6 +60,7 @@ export default class FormComponent extends React.Component {
     } else {
       $('input[type="submit"]').attr('disabled', true);
       this.setState({
+        validInput: 1,
         message: "Thank you! Please wait sometime till the analysis is done. This usually takes about 5 minutes."
       })
       this._fetchData();
--- src/components/progress-bar.js
+++ src/components/progress-bar.js
@@ -10,10 +10,17 @@ export default class ProgressBar extends React.Component {
   }
 
   render() {
-    return(
-      <div className="progress">
-        <div className="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow={this.state.progress} aria-valuemin="0" aria-valuemax="100" />
-      </div>
-    );
+    if (this.state.progress > 0 && this.state.progress < 100) {
+      return(
+        <div className="progress">
+          <div className="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow={this.state.progress} aria-valuemin="0" aria-valuemax="100" />
+        </div>
+      );
+    }
+    else {
+      return(
+        <div />
+      );
+    }
   }
 }
