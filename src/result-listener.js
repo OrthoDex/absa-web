6f3f3bd3d5fa2f77b1f9513f83fc0695d0931872
Ishaan Malhi <ishaan.malhi@gmail.com>
2017-08-26 14:41:03 +0530

    refactor: Move dev dependencies out from prod

--- src/result-listener.js
+++ src/result-listener.js
@@ -1,6 +1,5 @@
 import openSocket from 'socket.io-client';
-// const socket_url = process.env.NODE_ENV === 'production' ? window.location.href : 'http://localhost:5000';
-const socket_url = window.location.href;
+const socket_url = process.env.NODE_ENV === 'production' ? window.location.href : 'http://localhost:5000';
 const socket = openSocket.connect(socket_url);
 
 function subscribeToResults(callback) {
